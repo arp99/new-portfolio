e50ca30934e223c36eb32a961d38d8db30d24a63
Arpan Mondal <arpan19991025@gmail.com>
2021-11-25 19:23:01 +0530

    Modify button to add extra classes

--- Components/Button.js
+++ Components/Button.js
@@ -1,7 +1,7 @@
-export const Button = ({ children }) => {
+export const Button = ({ children, className }) => {
   return (
     <button
-      className="
+      className={`
             text-pallet1-navyBlue
             border 
             border-solid 
@@ -10,7 +10,9 @@ export const Button = ({ children }) => {
             flex items-center justify-evenly
             group hover:border-transparent
             relative
-        "
+            cursor-pointer
+            ${ className }
+        `}
     >
       <span className="w-0 h-full absolute left-0 top-0 group-hover:w-full transition-all ease-out duration-300 bg-pallet1-navyBlue"></span>
       <span className="group-hover:text-pallet1-babyBlue relative flex">{children}</span>
