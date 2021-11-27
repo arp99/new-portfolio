c69da62e0d919da405de32ba9120185ebd510cf6
Arpan Mondal <arpan19991025@gmail.com>
2021-11-27 15:40:18 +0530

    Feat:Add projects page

---
+++ pages/projects/index.js
@@ -0,0 +1,34 @@
+import { Card } from "../../Components/Card";
+
+export default function Projects() {
+  return (
+    <section className="w-full min-h-screen flex flex-col p-10 items-center justify-start bg-pallet1-blueGrotto">
+        <h1 className="text-4xl md:text-5xl font-black text-white text-center">
+          Projects
+        </h1>
+      <section className="w-full h-max mt-5 flex flex-col items-center drop-shadow-lg">
+        <Card
+          imgSrc="/assets/projects/plovex-ui.png"
+          title="Plovex UI"
+          description="A Lightweight Front-end library for easier and faster development of Web Interfaces"
+          liveLink="https://plovex-ui.netlify.app/"
+          sourceLink="https://github.com/arp99/Plovex-UI/tree/development"
+        />
+        <Card
+          imgSrc="/assets/projects/plovex-player.png"
+          title="Plovex Player"
+          description="A video library app for all travel enthusiasts, explore the world with these videos."
+          liveLink="https://plovex-player.netlify.app/"
+          sourceLink="https://github.com/arp99/plovex-player"
+        />
+        <Card
+          imgSrc="/assets/projects/plovex-store.png"
+          title="Plovex Store"
+          description="An E commerce app for all fashion Enthusiasts."
+          liveLink="https://plovex-store.netlify.app/"
+          sourceLink="https://github.com/arp99/plovex-store"
+        />
+      </section>
+    </section>
+  );
+}
